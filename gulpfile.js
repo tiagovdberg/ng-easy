ca1ffe657cef5a62731a526e2cfd1377a408d972
Tiago van den Berg <tiagovdberg@gmail.com>
2016-12-06 15:05:05 -0200

    Bump and tag is back

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var 
 	gulp = require('gulp'),
+	bump = require('gulp-bump'),
 	concat = require('gulp-concat'),
 	jshint = require('gulp-jshint'),
 	rename = require('gulp-rename'),
-	uglify = require('gulp-uglify');
+	tagVersion = require('gulp-tag-version'),
+	uglify = require('gulp-uglify'),
+	yargs = require('yargs');
 
 gulp.task('default', ['build']);
 gulp.task('build', ['js']);
@@ -17,5 +20,39 @@ gulp.task('js', function () {
 	.pipe(rename('ng-easy.min.js'))
 	.pipe(uglify())
 	.pipe(gulp.dest('dist'));
+});
+
+gulp.task('bump', function () {
+	/// <summary>
+	/// It bumps revisions
+	/// Usage:
+	/// 1. gulp bump : bumps the package.json and bower.json to the next prerelease revision.
+	///   i.e. from 0.1.1-25 to 0.1.1-26
+	/// 2. gulp bump --version 1.1.1 : bumps/sets the package.json and bower.json to the 
+	///	specified revision.
+	/// 3. gulp bump --type major	   : bumps 1.0.0 
+	///	gulp bump --type minor	   : bumps 0.1.0
+	///	gulp bump --type patch	   : bumps 0.0.2
+	///	gulp bump --type prerelease  : bumps 0.0.1-2
+	/// </summary>
+	var args = yargs.argv;
+	var type = args.type;
+	var version = args.version;
+	var options = {};
+	if (typeof version !== 'undefined') {
+		options.version = version;
+	} else if(typeof type !== 'undefined') {
+		options.type = type;
+	} else {
+		options.type = 'prerelease';
+	}
+	return gulp
+		.src(['package.json', 'bower.json'])
+		.pipe(bump(options))
+		.pipe(gulp.dest('./'));
+});
+
+gulp.task('tag', function() {
+	return gulp.src(['package.json']).pipe(tagVersion());
 });
 
