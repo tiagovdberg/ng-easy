0244d398c4c409ec8873545ae663d2f50c99e29f
Tiago van den Berg <tiagovdberg@gmail.com>
2019-09-02 02:12:32 -0300

    Angular style getters and setters

--- src/easy-controller/easy-controller.extfn.js
+++ src/easy-controller/easy-controller.extfn.js
@@ -18,87 +18,91 @@
 	};
 
 	angular.easy.registerFunction('easyController', function (module, config) {
-		ctrlRoute = (status)=>angular.isDefined(config.status[status].route) ? (((r = fnVal(config.route, self)) ? r : hyphenCase(ctrlFeature(config.name))) + fnVal(config.status[status].route, self) ) : undefined;
+		ctrlRoute = (st)=>angular.isDefined(config.status[st].route) ? (((r = fnVal(config.route, self)) ? r : hyphenCase(ctrlFeature(config.name))) + fnVal(config.status[st].route, self) ) : undefined;
 		
 		angular.module(module).config(['$controllerProvider', '$routeProvider', function($controllerProvider, $routeProvider) {
 			$controllerProvider.register(config.name, ['$location', '$route', '$routeParams', '$q', '$http', function($location, $route, $routeParams, $q, $http) {
 				var self = this;
-				self.getModel = ()=>self[self.status].model;
-				self.getData = ()=>self[self.status].data;
-				self.getVars = ()=>self[self.status].vars;
-				self.getParams = ()=>self[self.status].params;
-				self.getTemplateUrl = ()=>((t = fnVal(config.templateUrl, self)) ? t : hyphenCase(module) + '/' + hyphenCase(ctrlFeature(config.name))) + ((st = fnVal(config.status[self.status].templateUrl, self)) ? st : '/' + hyphenCase(trimPrefixes(self.status, ['put', 'get', 'post', 'delete'])) + '.html');
+				self.model = ()=>self[self.$status].model;
+				self.data = ()=>self[self.$status].data;
+				self.vars = ()=>self[self.$status].vars;
+				self.params = ()=>self[self.$status].params;
+				self.templateUrl = ()=>((t = fnVal(config.templateUrl, self)) ? t : hyphenCase(module) + '/' + hyphenCase(ctrlFeature(config.name))) + ((st = fnVal(config.status[self.$status].templateUrl, self)) ? st : '/' + hyphenCase(trimPrefixes(self.$status, ['put', 'get', 'post', 'delete'])) + '.html');
+				self.status = status;
 				
 				//init()
-				delete self.status;
-				angular.forEach(config.status, (stConfig, status)=>{
-					self[status] = angular.isFunction(stConfig) ? stConfig : angular.bind(self, goToStatus, status);
-					self[status].model = {};
-					self[status].data = {};
-					self[status].vars = {};
-					self[status].params = {};
+				delete self.$status;
+				angular.forEach(config.status, (stConfig, st)=>{
+					self[st] = angular.isFunction(stConfig) ? stConfig : angular.bind(self, status, st);
+					self[st].model = {};
+					self[st].data = {};
+					self[st].vars = {};
+					self[st].params = {};
 				});
-				for (var status in config.status) { 
-					if (!config.status.hasOwnProperty(status)) continue;
-					if (angular.isUndefined((cr = ctrlRoute(status))) || $route.current.originalPath !== cr) continue; 
-					self[status]();
+				for (var st in config.status) { 
+					if (!config.status.hasOwnProperty(st)) continue;
+					if (angular.isUndefined((cr = ctrlRoute(st))) || $route.current.originalPath !== cr) continue; 
+					self[st]();
 					return;
 				}					
-				function goToStatus(status, form) {
-					//Messages.clearMessages();
-					//if (angular.isDefined(form) && Messages.formErrors(self.getTemplateUrl(), form)) return $q.defer().resolve();
-					
-					if(angular.isUndefined(self.status)) {
-						self.status = status;
+				
+				function status(st, form) {
+					if(angular.isUndefined(st)) {
+						return self.$status;
 					}
-					var cr = ctrlRoute(status);
-					if (status != self.status && angular.isDefined(cr)) { 
-						var params = angular.copy(fnVal(config.status[status].routeParams, self), {});
+					
+					//Messages.clearMessages();
+					//if (angular.isDefined(form) && Messages.formErrors(self.getTemplateUrl(), form)) return $q.resolve({data : {}});
+					
+					if(angular.isUndefined(self.$status)) {
+						self.$status = st;
+					}
+					var cr = ctrlRoute(st);
+					if (st != self.$status && angular.isDefined(cr)) { 
+						var params = angular.copy(fnVal(config.status[st].routeParams, self), {});
 						$location.path(interpolatePath(cr, params));
 						$location.search(params);
-						return;
-//						return $q.resolve({data : {}});
+						return $q.resolve({data : {}});
 					} 
 		
-					var method = (a = ['PUT','GET','DELETE','POST'].filter(m=>status.toUpperCase().startsWith(m))).length === 1 ? a[0] : false; 
+					var method = (a = ['PUT','GET','DELETE','POST'].filter(m=>st.toUpperCase().startsWith(m))).length === 1 ? a[0] : false; 
 					if(!method) {
-						return;
-//						return $q.resolve({data : {}}, (fn = config.status[status].success) ? angular.bind(self, fn, self) : angular.bind(self, success, self) );
+						return $q.resolve({data : {}}, (fn = config.status[st].success) ? angular.bind(self, fn, self) : angular.bind(self, success, self) );
 					}
 					
 					return $http({
 						method: method,
-						data: self.getModel(),
-						url: (u = fnVal(config.status[status].serviceUrl, self)) ? u : "/api" + $location.url()
+						data: self.model(),
+						url: (u = fnVal(config.status[st].serviceUrl, self)) ? u : "/api" + $location.url()
 					}).then(
-						(fn = config.status[status].success) ? angular.bind(self, fn, self) : angular.bind(self, success, self), 
-						(fn = config.status[status].fail) ? angular.bind(self, fn, self) : angular.bind(self, fail, self)
+						(fn = config.status[st].success) ? angular.bind(self, fn, self) : angular.bind(self, success, self), 
+						(fn = config.status[st].fail) ? angular.bind(self, fn, self) : angular.bind(self, fail, self)
 					);
 		
 					//TODO model and data must accept functions with response as argument.
 					function success(self, response) {
-						var newStatus = fnVal(config.status[status].statusOnSuccess, self) || fnVal(config.status[status].status, self) || status;
+						var newStatus = fnVal(config.status[st].statusOnSuccess, self) || fnVal(config.status[st].status, self) || st;
 		
 						self[newStatus].data = response.data;
-						angular.copy(fnVal(config.status[status].modelOnSuccess, self) || fnVal(config.status[status].model, self), self[newStatus].model);
-						angular.copy(fnVal(config.status[status].varsOnSuccess, self) || fnVal(config.status[status].vars, self), self[newStatus].vars);
+						angular.copy(fnVal(config.status[st].modelOnSuccess, self) || fnVal(config.status[st].model, self), self[newStatus].model);
+						angular.copy(fnVal(config.status[st].varsOnSuccess, self) || fnVal(config.status[st].vars, self), self[newStatus].vars);
 		
-						if(newStatus === self.status) return;
+						if(newStatus === self.$status) return;
 						
-						self.status = newStatus;
+						self.$status = newStatus;
 						self[newStatus]();
 					}
 					
 					function fail(self, response) {
-						var newStatus = fnVal(config.status[status].statusOnFail, self) || fnVal(config.status[status].status, self) || status;
+						var newStatus = fnVal(config.status[st].statusOnFail, self) || fnVal(config.status[st].status, self) || st;
 		
 						self[newStatus].data = response.data;
-						angular.copy(fnVal(config.status[status].modelOnFail, self) || fnVal(config.status[status].model, self), self[newStatus].model);
-						angular.copy(fnVal(config.status[status].varsOnFail, self) || fnVal(config.status[status].vars, self), self[newStatus].vars);
+						angular.copy(fnVal(config.status[st].modelOnFail, self) || fnVal(config.status[st].model, self), self[newStatus].model);
+						angular.copy(fnVal(config.status[st].varsOnFail, self) || fnVal(config.status[st].vars, self), self[newStatus].vars);
 		
-						var statusChanged = newStatus !== status || newStatus != self.status;
-						self.status = newStatus;
-						if(statusChanged) {
+						var changed = newStatus !== st || newStatus != self.$status;
+						self.$status = newStatus;
+						if(changed) {
 							self[newStatus]();
 						}
 		//				Messages.handleErrors(response);
@@ -110,13 +114,13 @@
 				}
 			}]); //$controllerProvider.register(
 					
-			angular.forEach(config.status, (stConfig, status)=>{
+			angular.forEach(config.status, (stConfig)=>{
 				if (angular.isUndefined(stConfig.route)) return;
 				var ctrlScope = ctrlFeature(config.name) + 'Ctrl';
 				$routeProvider.when(config.route + stConfig.route, {
 					controller : config.name,
 					controllerAs : ctrlScope,
-					template : '<ng:include src="' + ctrlScope + '.getTemplateUrl()"></ng:include>'
+					template : '<ng:include src="' + ctrlScope + '.templateUrl()"></ng:include>'
 				});
 			});
 		}]); //angular.module(module).config(
