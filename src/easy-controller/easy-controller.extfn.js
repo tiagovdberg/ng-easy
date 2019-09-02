b641ea87ac3c772cd829b4c839693abb88d98e59
Tiago van den Berg <tiagovdberg@gmail.com>
2019-09-02 01:31:03 -0300

    Bug Fixes in routes.

--- src/easy-controller/easy-controller.extfn.js
+++ src/easy-controller/easy-controller.extfn.js
@@ -4,20 +4,39 @@
 //TODO InitialStatus has a default if there is only one state.
 
 (function() {
+	//fnVal = (v, self)=>angular.isFunction(v) ? v.apply(self, self) : v;
+	
+	function fnVal(v /*, arguments[1:] */) {
+		if (angular.isFunction(v)) {
+			var extraArgs = Array.prototype.slice.call(arguments, 1);
+			return v.apply(this, extraArgs);
+		}
+		return v;
+	}
 
-	var angularEasy = angular.easy;
-	angularEasy.registerFunction('easyController', function (module, config) {
+	camelCase = s=>s.substring(0,1).toLowerCase() + s.substring(1);
+	hyphenCase = s=>s.split('').map((c,i)=>(l = c.toLowerCase()) !== c && i > 0 ? '-' + l : l).join('');
+	ctrlFeature = s=>camelCase(trimSuffix(s, 'Controller'));
+	trimPrefix = (s, prefix)=>s.startsWith(prefix) ? s.substring(prefix.length) : s; 
+	trimSuffix = (s, suffix)=>s.endsWith(suffix) ? s.substring(0, s.length - suffix.length) : s;
+	trimPrefixes = (s, prefixes)=>{
+		for(var x = 0; x < prefixes.length; x++) {
+			if( (ts = trimPrefix(s, prefixes[x])) !== s) return ts;
+		}
+		return s;
+	};
+
+	angular.easy.registerFunction('easyController', function (module, config) {
+		ctrlRoute = (status)=>angular.isDefined(config.status[status].route) ? (((r = fnVal(config.route, self)) ? r : hyphenCase(ctrlFeature(config.name))) + fnVal(config.status[status].route, self) ) : undefined;
 		
 		angular.module(module).config(['$controllerProvider', '$routeProvider', function($controllerProvider, $routeProvider) {
 			$controllerProvider.register(config.name, ['$location', '$route', '$routeParams', '$q', '$http', function($location, $route, $routeParams, $q, $http) {
 				var self = this;
-				
 				self.getModel = ()=>self[self.status].model;
 				self.getData = ()=>self[self.status].data;
 				self.getVars = ()=>self[self.status].vars;
 				self.getParams = ()=>self[self.status].params;
-				self.getTemplateUrl = ()=>((t = fnVal(config.templateUrl, self)) ? t : dashedCase(module) + '/' + dashedCase(ctrlFeature(config.name))) + ((st = fnVal(config.status[self.status].templateUrl, self)) ? st : '/' + dashedCase(self.status) + '.html');
-//				self.getTemplateUrl = ()=>dashedCase(module) + '/' + dashedCase(config.name) + '/' + dashedCase(self.status) + '.html';
+				self.getTemplateUrl = ()=>((t = fnVal(config.templateUrl, self)) ? t : hyphenCase(module) + '/' + hyphenCase(ctrlFeature(config.name))) + ((st = fnVal(config.status[self.status].templateUrl, self)) ? st : '/' + hyphenCase(trimPrefixes(self.status, ['put', 'get', 'post', 'delete'])) + '.html');
 				
 				//init()
 				delete self.status;
@@ -27,21 +46,13 @@
 					self[status].data = {};
 					self[status].vars = {};
 					self[status].params = {};
-					self[status].route = {}; //TODO
 				});
-				angular.forEach($route.routes, (route, path)=>{
-					if(!angular.isUndefined(route.regexp) && route.regexp.exec($location.url())) { 
-						angular.forEach(config.status, (stConfig, status)=>{
-							//TODO Eval Defauts config.route + stConfig.route
-							//>((r = fnVal(config.route, self)) ? r : dashedCase(module) + '/' + dashedCase(ctrlName)) + (sr = fnVal(config.status.[self.status].route, self)) ? sr : '/' + dashedCase(self.status) + '.html';
-							if(path === (config.route + stConfig.route)) {
-								self[status]();
-								return;
-							}
-						});
-					}
-				});					
-				
+				for (var status in config.status) { 
+					if (!config.status.hasOwnProperty(status)) continue;
+					if (angular.isUndefined((cr = ctrlRoute(status))) || $route.current.originalPath !== cr) continue; 
+					self[status]();
+					return;
+				}					
 				function goToStatus(status, form) {
 					//Messages.clearMessages();
 					//if (angular.isDefined(form) && Messages.formErrors(self.getTemplateUrl(), form)) return $q.defer().resolve();
@@ -49,31 +60,32 @@
 					if(angular.isUndefined(self.status)) {
 						self.status = status;
 					}
-					
-//					if (angular.isDefined(config.status[status].route) && //TODO) {
-//						var params = angular.copy($routeParams, {});
-//						$location.path(interpolatePath(config.routeBase + config.status[status].route, params));
-//						$location.search(params);
-//						return $q.defer().resolve();
-//					} 
+					var cr = ctrlRoute(status);
+					if (status != self.status && angular.isDefined(cr)) { 
+						var params = angular.copy(fnVal(config.status[status].routeParams, self), {});
+						$location.path(interpolatePath(cr, params));
+						$location.search(params);
+						return;
+//						return $q.resolve({data : {}});
+					} 
 		
 					var method = (a = ['PUT','GET','DELETE','POST'].filter(m=>status.toUpperCase().startsWith(m))).length === 1 ? a[0] : false; 
 					if(!method) {
-						success({data : {}});
-						return $q.defer().resolve();
+						return;
+//						return $q.resolve({data : {}}, (fn = config.status[status].success) ? angular.bind(self, fn, self) : angular.bind(self, success, self) );
 					}
 					
 					return $http({
 						method: method,
 						data: self.getModel(),
-						url: (u = fnVal(config.status[self.status].templateUrl, self)) ? u : "/api/" + $location.url()
+						url: (u = fnVal(config.status[status].serviceUrl, self)) ? u : "/api" + $location.url()
 					}).then(
-						(fn = config.status[status].success) ? fn : success, 
-						(fn = config.status[status].fail) ? fn : fail
+						(fn = config.status[status].success) ? angular.bind(self, fn, self) : angular.bind(self, success, self), 
+						(fn = config.status[status].fail) ? angular.bind(self, fn, self) : angular.bind(self, fail, self)
 					);
 		
 					//TODO model and data must accept functions with response as argument.
-					function success(response) {
+					function success(self, response) {
 						var newStatus = fnVal(config.status[status].statusOnSuccess, self) || fnVal(config.status[status].status, self) || status;
 		
 						self[newStatus].data = response.data;
@@ -82,21 +94,21 @@
 		
 						if(newStatus === self.status) return;
 						
-						self.status = statusOnSuccess;
-						self[statusOnSuccess]();
+						self.status = newStatus;
+						self[newStatus]();
 					}
 					
-					function fail(response) {
+					function fail(self, response) {
 						var newStatus = fnVal(config.status[status].statusOnFail, self) || fnVal(config.status[status].status, self) || status;
 		
 						self[newStatus].data = response.data;
 						angular.copy(fnVal(config.status[status].modelOnFail, self) || fnVal(config.status[status].model, self), self[newStatus].model);
 						angular.copy(fnVal(config.status[status].varsOnFail, self) || fnVal(config.status[status].vars, self), self[newStatus].vars);
 		
-						var statusChanged = newStatus !== status;
+						var statusChanged = newStatus !== status || newStatus != self.status;
 						self.status = newStatus;
 						if(statusChanged) {
-							self[statusOnFail]();
+							self[newStatus]();
 						}
 		//				Messages.handleErrors(response);
 		//				var messageOnFail = fnVal(effectiveConfigStatus[newStatusName].messageOnFail, self);
@@ -108,52 +120,16 @@
 			}]); //$controllerProvider.register(
 					
 			angular.forEach(config.status, (stConfig, status)=>{
-				if (angular.isDefined(stConfig.route)) {
-					var ctrlScope = ctrlFeature(config.name) + 'Ctrl';
-					$routeProvider.when(config.route + stConfig.route, {
-						controller : config.name,
-						controllerAs : ctrlScope,
-						template : '<ng:include src="' + ctrlScope + '.getTemplateUrl()"></ng:include>'
-					});
-				}
+				if (angular.isUndefined(stConfig.route)) return;
+				var ctrlScope = ctrlFeature(config.name) + 'Ctrl';
+				$routeProvider.when(config.route + stConfig.route, {
+					controller : config.name,
+					controllerAs : ctrlScope,
+					template : '<ng:include src="' + ctrlScope + '.getTemplateUrl()"></ng:include>'
+				});
 			});
 		}]); //angular.module(module).config(
 	});
-	function fnVal(v, self) {
-		return angular.isFunction(v) ? v.apply(self, arguments.slice(arguments, 2)) : v;
-	}
-	
-	function camelCase(s) {
-		return s.substring(0,1).toLowerCase() + s.substring(1);
-	}
-
-	function dashedCase(s) {
-		return s.split('').map((c,i)=>(l = c.toLowerCase()) !== c && i > 0 ? '-' + l : l).join('');
-	}
-	
-	function ctrlFeature(s) {
-		return camelCase(trimSuffix(s, ['Controller', 'Ctrl', 'Ctl']));
-	}
-	
-	function trimPrefix(name, knownPrefixes) {
-		for (var knownPrefixIndex = 0; knownPrefixIndex < knownPrefixes.length; knownPrefixIndex++) {
-			var knownPrefix = knownPrefixes[knownPrefixIndex];
-			if (name.toLowerCase().startsWith(knownPrefix.toLowerCase())) {
-				return name.substring(knownPrefix.length);
-			}
-		}
-		return name;
-	}
-
-	function trimSuffix(name, knownSuffixes) {
-		for (var knownSuffixIndex = 0; knownSuffixIndex < knownSuffixes.length; knownSuffixIndex++) {
-			var knownSuffix = knownSuffixes[knownSuffixIndex];
-			if (name.toLowerCase().endsWith(knownSuffix.toLowerCase())) {
-				return name.substring(0, name.length - knownSuffix.length);	
-			}
-		}
-		return name;
-	}
 
 	function interpolatePath(string, params) {
 		var result = [];
